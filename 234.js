dab09b02b78411099ee708b3719f5404aedf4fb8
Mehovuhka <97909063+Mehovuhka@users.noreply.github.com>
2022-03-04 00:07:04 +0300

    События

--- 234.js
+++ 234.js
@@ -1,23 +1,87 @@
-let arrayOp = []
-for(let index = 0; index < 5; index++){
-    arrayOp[index] = {
-        value: `co${index + 1}`,
-        label: `Вариант ${index + 1}`,
-    }
-}
-let selected = document.createElement("select")
-let selReady = initOpt(arrayOp)
-document.body.append(selReady)
-function initOpt(arrayOp){
-    opArr = []
-    for (let index in arrayOp){
-        opArr[index] = document.createElement("option")
-    } 
-    console.log(opArr)
-    for (let index in opArr){
-        opArr[index].textContent = arrayOp[index].label
-        opArr[index].value = arrayOp[index].value
-        selected.append(opArr[index])
-    }
-    return selected
-}
+// 1 задание
+// let zad1 = document.querySelector('.zad1')
+// zad1.onclick = () => {
+//     alert("Hello world!")
+// }
+    
+// 2 задание
+// function buttonClick() {
+// 	let input = document.getElementById('input');
+// 	input.value = 'test@gmail.com';
+// }
+// 3 задание
+// let input = document.querySelector("input")
+// let button = document.querySelector(".zad3")
+
+// button.addEventListener("click",() => {
+//     let value = input.value()
+//     if (value){
+//       alert(`Вы ввели : ${value}`)
+//     } 
+//     else{
+//       alert("Вы ничего не ввели в поле")
+//     }  
+// })
+
+
+// 4 задание
+// let but1 = document.querySelector('.zad41')
+// let but2 = document.querySelector('.zad42')
+
+// but1.addEventListener('click', () =>{
+//     let obj1 = document.getElementById("left")
+//     let obj2 = document.getElementById("right")
+  
+//     let temp = obj1.value;
+//     obj1.value = obj2.value;
+//     obj2.value = temp;
+// })
+// but2.addEventListener('click', () =>{
+//     document.getElementById('left').value = ''
+//     document.getElementById('right').value = ''
+// })
+
+// 5 задание
+// let but1 = document.querySelector('.block')
+// let but2 = document.querySelector('.anblock')
+
+// but1.addEventListener('click', () =>{
+//     let elem = document.querySelector('#elem');
+//     alert(elem.disabled);
+// })
+
+// but2.addEventListener('click', () =>{
+//     let elem = document.querySelector('#elem');
+//     elem.disabled = false;
+// })
+
+// 6 задание
+// let but1 = document.querySelector('.show')
+// let but2 = document.querySelector('.hide')
+// let square = document.querySelector('.container')
+// but2.addEventListener('click',() => {
+//   if (square.style.display == 'block'){
+//     square.style.display = 'none'
+//   }
+// })
+// but1.addEventListener('click',() => {
+//     square.style.display = 'block'
+// })
+
+
+// 7 задание
+// let con = document.querySelector('.container')
+// con.addEventListener('mouseover',()=>{
+//     con.style.backgroundColor = "green"
+// })
+// con.addEventListener('mouseout',()=>{
+//     con.style.backgroundColor = "red"
+// })
+
+
+// 8 задание
+
+
+
+
+// 9 задание
